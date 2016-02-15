7f5a0ea8354d0977b472efc054c62a123f32567d
Jan Ycasas <ycasas.jan@gmail.com>
2016-02-15 14:29:45 -0800

    Initial Commit

---
+++ DiplomaDataModel/Option/Option.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaDataModel.Option
+{
+    public class Option
+    {
+        [Key]
+        public int OptionId { get; set; }
+        [MaxLength(50)]
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
